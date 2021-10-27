fca05029143bf0362b886be5c8b7fec837d7c441
ky1004 <pengukim@naver.com>
2021-10-27 12:59:51 +0900

    2. safe file

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -21,15 +21,67 @@ namespace NumberGuesser
         // Entry Point Method
         static void Main(string[] args)
         {
-            string name = "hina";
-            int age = 17;
+           // Set app vars
+           string appName = "Number Guesser";
+           string appVersion = "1.0.0";
+           string appAuthor = "Brad Traversy";
 
-            /* start */
-            //Console.Write();
-            Console.WriteLine("name "+ name);
-            Console.WriteLine("name "+ age);
-            Console.WriteLine("{0} is {1}", name, age);
+           // Change text color
+           Console.ForegroundColor = ConsoleColor.Green;
 
+           // Write out app info
+           Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+
+           // Reset text color
+           Console.ResetColor();
+
+           // Ask users name
+           Console.WriteLine("What is your name??");
+
+           // Get user input
+           string inputName = Console.ReadLine();
+           Console.WriteLine("Hello {0}, let's play a game...", inputName);
+           
+           // Set correct number
+           int correctNumber = 7;
+
+           // Init guess var
+           int guess = 0;
+           
+           // Ask user for number
+           Console.WriteLine("Guess a number between 1 and 10");
+
+           // While guess is not correct
+           while(guess != correctNumber){
+               // Get users input
+               string input = Console.ReadLine();
+
+               // Cast to int and put in guess
+               guess = Int32.Parse(input);
+
+               // Match guess to correct number
+               if(guess != correctNumber){
+                    // Change text color
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    // Write out app info
+                    Console.WriteLine("Wrong Number...Try Again");
+    
+                    // Reset text color
+                    Console.ResetColor();
+               }
+               else{
+                    // Change text color
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                    // Write out app info
+                    Console.WriteLine("Congratulation!!!");
+    
+                    // Reset text color
+                    Console.ResetColor();
+                    
+               }
+           }
         }
     }
 }
