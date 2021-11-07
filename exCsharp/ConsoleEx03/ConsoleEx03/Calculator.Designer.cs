c7d764a7338d016571e5de77be418c03518d4697
HappyHina <34612499+ky1004@users.noreply.github.com>
2021-11-08 08:56:33 +0900

    Ex03_update file

---
+++ exCsharp/ConsoleEx03/ConsoleEx03/Calculator.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace ConsoleEx03
+{
+    partial class Calculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NumButton1 = new System.Windows.Forms.Button();
+            this.NumButton2 = new System.Windows.Forms.Button();
+            this.NumScreen = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NumButton1
+            // 
+            this.NumButton1.Location = new System.Drawing.Point(12, 96);
+            this.NumButton1.Name = "NumButton1";
+            this.NumButton1.Size = new System.Drawing.Size(53, 54);
+            this.NumButton1.TabIndex = 0;
+            this.NumButton1.Text = "1";
+            this.NumButton1.UseVisualStyleBackColor = true;
+            this.NumButton1.Click += new System.EventHandler(this.NumButton1_Click);
+            // 
+            // NumButton2
+            // 
+            this.NumButton2.Location = new System.Drawing.Point(83, 96);
+            this.NumButton2.Name = "NumButton2";
+            this.NumButton2.Size = new System.Drawing.Size(53, 54);
+            this.NumButton2.TabIndex = 1;
+            this.NumButton2.Text = "2";
+            this.NumButton2.UseVisualStyleBackColor = true;
+            this.NumButton2.Click += new System.EventHandler(this.NumButton2_Click);
+            // 
+            // NumScreen
+            // 
+            this.NumScreen.BackColor = System.Drawing.Color.White;
+            this.NumScreen.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.NumScreen.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.NumScreen.Location = new System.Drawing.Point(12, 9);
+            this.NumScreen.Name = "NumScreen";
+            this.NumScreen.Size = new System.Drawing.Size(386, 33);
+            this.NumScreen.TabIndex = 2;
+            this.NumScreen.Text = "0";
+            this.NumScreen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Calculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 346);
+            this.Controls.Add(this.NumScreen);
+            this.Controls.Add(this.NumButton2);
+            this.Controls.Add(this.NumButton1);
+            this.Name = "Calculator";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NumButton1;
+        private System.Windows.Forms.Button NumButton2;
+        private System.Windows.Forms.Label NumScreen;
+    }
+}
+
---
+++ exCsharp/ConsoleEx03/ConsoleEx03/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleEx03
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        // 함수의 프로토타입, 선언, 정의
+        //steamedrice Cook(rice, water) {  }
+        public int Add(int number1, int number2)
+        {
+            // int라는 결과물을 내지 않으면 이 함수는 정의 자체가 실패가 된다
+            int sum = number1 + number2;
+            // 결과물을 만들어내는 키워드 return
+            return sum;
+        }
+
+        public float Add2(float number1, float number2)
+        {
+            float sum = number1 + number2;
+            return sum;
+        }
+
+        public int Sub(int number1, int number2)
+        {
+            int sub = number1 - number2;
+            return sub;
+        }
+
+        private void NumButton1_Click(object sender, EventArgs e)
+        {
+            // 만약 NumScreen의 텍스트가 0이라면 1로 변경
+            if(NumScreen.Text == "0")
+                NumScreen.Text = "1";
+            else
+                // 기존에 있는 숫자를 버리지 않고 실행
+                NumScreen.Text += "1";
+        }
+
+        private void NumButton2_Click(object sender, EventArgs e)
+        {
+            // 만약 NumScreen의 텍스트가 0이라면 2로 변경
+            if (NumScreen.Text == "0")
+                NumScreen.Text = "2";
+            else
+                // 기존에 있는 숫자를 버리지 않고 실행
+                NumScreen.Text += "2";
+        }
+
+    }
+}
