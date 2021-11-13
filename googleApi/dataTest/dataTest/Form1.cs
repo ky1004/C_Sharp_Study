421f85ce24b27cf19c2dbae4a45d66ab6035e67f
HappyHina <34612499+ky1004@users.noreply.github.com>
2021-11-13 23:16:41 +0900

    update file

---
+++ googleApi/dataTest/dataTest/Form1.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dataTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        async void btnSearch_Click(object sender, EventArgs e)
+        {
+            // YouTubeService 객체 생성
+            var youtube = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApiKey = "", // 키 지정
+                ApplicationName = "My YouTube Search"
+            });
+
+            // Search용 Request 생성
+            var request = youtube.Search.List("snippet");
+            request.Q = txtSearch.Text;  //ex: "검색어"
+            request.MaxResults = 25;
+
+            // Search용 Request 실행
+            var result = await request.ExecuteAsync();
+
+            // Search 결과를 리스트뷰에 담기
+            foreach (var item in result.Items)
+            {
+                if (item.Id.Kind == "youtube#video")
+                {
+                    //listView1.View = View.List;
+                    listView1.Items.Add(item.Id.VideoId.ToString(), item.Snippet.Title, 0);
+
+
+                }
+            }
+        }
+
+
+    }
+}
